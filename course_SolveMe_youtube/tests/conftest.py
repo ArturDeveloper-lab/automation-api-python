551a0d2dedfeb48baee7de1902badc85560410aa
ArturBoltyansky <artur.boltiansky79@gmail.com>
2024-03-22 21:00:39 +0300

    fix

--- course_SolveMe_youtube/tests/conftest.py
+++ course_SolveMe_youtube/tests/conftest.py
@@ -1,13 +1,16 @@
+from random import randrange
+
 import pytest
 
-from random import randrange
 
 @pytest.fixture
 def get_number():
-    return randrange(1,1000,5)
+    return randrange(1, 1000, 5)
 
-def _calculator(a,b):
-    return a+b
+
+def _calculator(a, b):
+    return a + b
+
 
 @pytest.fixture
 def calculate():
